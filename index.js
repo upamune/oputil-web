90ad529a6f82bc1e8c669b615f407dca9718e891
Yu SERIZAWA <upamune@gmail.com>
2021-12-13 15:21:02 +0900

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+(() => {
+    let withDoubleQuote = false;
+
+    document.getElementById('input').addEventListener('change', function (e) {
+        update();
+    });
+
+    document.getElementById('with-double-quote').addEventListener('change', function (e) {
+        withDoubleQuote = e.target.checked;
+        update();
+    });
+
+    function update() {
+        const value = document.getElementById('input').value;
+        const output = document.getElementById('output');
+        output.textContent = convert(value);
+    }
+
+    function convert(value) {
+        let v = value
+            .trim()
+            .split(/\r?\n/);
+
+        if (withDoubleQuote) v = v.map(v => `"${v}"`)
+        return v.join(",");
+    }
+})();
